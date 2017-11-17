a06e7f1daf824c6a32f5c86c4230b37620223cfc
erikzaksf <erik.zak.sf@gmail.com>
2017-11-17 15:09:55 -0800

    add products with most reviews scope to welcome page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,13 @@ class Product < ActiveRecord::Base
   has_many :reviews
 
   scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost ,products.origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(2)
+    )}
 
   validates :name, :presence => true
   validates :cost, :presence => true
